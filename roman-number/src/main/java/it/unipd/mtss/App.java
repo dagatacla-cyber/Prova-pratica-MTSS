d11d63b95568ed3d8f3dafd53cc07bc175a67e44
dagatacla-cyber <dagata.cla@gmail.com>
2026-05-07 23:49:12 +0200

    Remove App class and main method
    Removed the main class and its method that printed 'Hello World'.

--- roman-number/src/main/java/it/unipd/mtss/App.java
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -3,15 +3,3 @@
 // [CLAUDIA] [D'AGATA] [2102259]
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
