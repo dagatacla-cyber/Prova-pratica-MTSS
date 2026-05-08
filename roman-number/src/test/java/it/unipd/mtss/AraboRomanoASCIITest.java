98c79ec2faf9e12aaf190dd2f342965779a00763
Claudia <dagata.cla@gmail.com>
2026-05-08 18:05:44 +0200

    aumentato coverage conversione arabo-romano

--- roman-number/src/test/java/it/unipd/mtss/AraboRomanoASCIITest.java
+++ roman-number/src/test/java/it/unipd/mtss/AraboRomanoASCIITest.java
@@ -108,11 +108,36 @@ public class AraboRomanoASCIITest {
     }
 
 
-    // test di un caso concomplesso: più regole di combinazione
+    // test con più regole di combinazione
+
+    @Test
+    void input_49_output_XLIX() {
+        assertEquals("XLIX", AraboRomanoASCII.convert(49));
+    }
+
+    @Test
+    void input_99_output_XCIX() {
+        assertEquals("XCIX", AraboRomanoASCII.convert(99));
+    }
 
     @Test
     void input_1994_output_MCMXCIV() {
         assertEquals("MCMXCIV", AraboRomanoASCII.convert(1994));
     }
+
+    // test di input non validi
+
+        @Test
+    void input_0_throws_exception() {
+        assertThrows(IllegalArgumentException.class,
+            () -> AraboRomanoASCII.convert(0));
+    }
+
+    @Test
+    void input_negative_throws_exception() {
+        assertThrows(IllegalArgumentException.class,
+            () -> AraboRomanoASCII.convert(-5));
+    }
+    
 
 }
